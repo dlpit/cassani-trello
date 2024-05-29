3791f50057b9291c0b585f0f50934d9493a2b501
Huy Sad Nhan <cv.nhuytyl2603@gmail.com>
2024-05-29 21:50:44 +0700

    Fix BUILD_MODE dev and prod

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,2 +1,11 @@
-export const API_ROOT = 'https://cassani-api.onrender.com'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://cassani-api.onrender.com'
+}
+
+export const API_ROOT = apiRoot
 // export const API_ROOT = 'http://localhost:8017'
