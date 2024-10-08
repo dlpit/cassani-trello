69aa001d44ecc6ad66732a65f07cbac7ad2820cb
nqhuy2603 <cv.nhuytyl2603@gmail.com>
2024-10-08 17:25:40 +0700

    complete config redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { activeBoardReducer } from '~/redux/activeBoard/activeBoardSlice'
 import { userReducer } from '~/redux/user/userSlice'
+import { combineReducers } from 'redux' // Có sẵn trong @reduxjs/toolkit
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const rootPersistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['user'] // only user will be persisted
+}
+
+const reducers = combineReducers({
+  activeBoard: activeBoardReducer,
+  user: userReducer
+})
+
+const persistedReducer = persistReducer(rootPersistConfig, reducers)
 
 export const store = configureStore({
-  reducer: {
-    activeBoard: activeBoardReducer,
-    user: userReducer
-  }
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false })
 })
