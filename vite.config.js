3117b2a23d77f95b6e3a8335b7ffc7a323e85527
nqhuy2603 <cv.nhuytyl2603@gmail.com>
2025-04-17 20:59:46 +0700

    fix index.html vite.conifig vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgr from 'vite-plugin-svgr'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +7,8 @@ export default defineConfig({
   define: {
     'process.env': process.env
   },
-  plugins: [
-    react(),
-    svgr()
-  ],
-  // base: './'
+  plugins: [react()],
+  base: '/',
   resolve: {
     alias: [
       { find: '~', replacement: '/src' }
